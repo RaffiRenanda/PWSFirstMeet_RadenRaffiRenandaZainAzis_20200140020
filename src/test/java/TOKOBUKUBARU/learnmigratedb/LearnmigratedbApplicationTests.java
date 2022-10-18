bd2fcbcbc54bef52fd43911d4b37166bb55020f1
RaffiRenanda <raden.raffi.ft20@mail.umy.ac.id>
2022-10-18 14:44:59 +0700

    first commit

---
+++ src/test/java/TOKOBUKUBARU/learnmigratedb/LearnmigratedbApplicationTests.java
@@ -0,0 +1,13 @@
+package TOKOBUKUBARU.learnmigratedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnmigratedbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
